fae28d2b6912934d87dca04b1f097eced0f87311
patriani <43487367+patriani@users.noreply.github.com>
2019-11-14 17:26:47 -0300

    Update HashConcorrente.java

--- HashConcorrente.java
+++ HashConcorrente.java
@@ -1,6 +1,17 @@
 import java.io.*;
 
 public class HashConcorrente extends ArquivoConcorrente implements Runnable{
+    private long t1 = 0;
+
+    public void setT1(long t1){
+        this.t1 = t1;
+    }
+
+    public long getT1(){
+        return t1;
+    }
+
+    public HashConcorrente(){} // construtor default
 
     public HashConcorrente(String or){
         super(or);
@@ -11,8 +22,10 @@ public class HashConcorrente extends ArquivoConcorrente implements Runnable{
 
         HashConcorrente2 tam = new HashConcorrente2();
 
+        setT1(System.currentTimeMillis()); //marca tempo de inicio da operação.
+
         File f = new File (getOr());
-        long tamanho = f.length();
+        long tamanho = f.length(); // grava tamanho do arquivo original
         tam.setTamanho(tamanho);        
     }
     
