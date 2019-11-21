61ec3e8edbf36fd4ccabc4b4028b395687e2ba22
patriani <43487367+patriani@users.noreply.github.com>
2019-11-20 21:55:54 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,38 +1,36 @@
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Principal{
-    public static void main(String[]args) throws IOException{
-        
+    public static void main(String[]args) throws IOException, IOException{
+
         String or; // recebe diretório de origem do backup
         String dest; // recebe diretório de destino do backup
         int aux = 1; // recebe 0 se não há mais arquivos a serem copiados
-        long t1 = 0, t2 = 0;
 
         Scanner sc = new Scanner(System.in);
 
-        Arquivo ar = new Arquivo();
-
         while(aux != 0){
-        
-        //Interação com o usuário -- inserir diretório de origem e de destino
-        System.out.println("Insira o diretorio qualquer de origem (especificando o tipo do arquivo - .pdf,...)");
-        or = sc.nextLine();
-        System.out.println("Insira o diretorio qualquer de destino");
-        dest = sc.nextLine();
 
-        t1 = System.currentTimeMillis();
-        ar.transf(or,dest); // chama método para copiar o original e colar no diretorio informado como destino
-        t2 = System.currentTimeMillis();
+            System.out.println("Insira o diretorio de origem");
+            or = sc.nextLine();
+            System.out.println("Insira o diretorio de destino");
+            dest = sc.nextLine();
 
-        System.out.println("\n Tempo: " +(t2-t1));
-
-        System.out.println("Deseja fazer backup de mais algum arquivo ?\n [1 - sim/ 0 - nao]: ");
-        aux = sc.nextInt(); 
-        sc.nextLine();
-
+            Transferencia Tr = new Transferencia(or,dest); // Passa diretorio de origem e de destino como parametros
+            Thread Ttr = new Thread(Tr);
+    
+            //Delete Dl = new Delete(); // Passa o diretorio de destino como parametro 
+            //hread Tdl = new Thread(Dl);
+            
+            Ttr.start(); // inicia thread para cópia e "paste" em outro diretorio
+            
+            //Tdl.start(); // inicia thread para exclusão do arquivo copiado
+            
+            System.out.println("Deseja fazer backup de mais algum arquivo ?\n [1 - sim/ 0 - nao]: ");
+            aux = sc.nextInt();
         }
+        sc.close();
     }
+    
 }
